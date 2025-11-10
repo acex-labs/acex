4a220a65dc5f28f0e04f009b243d83d230f9486a
jani@naamanka.se <jani@naamanka.se>
2025-11-10 16:30:11 +0100

    Initial commit of aaa

---
+++ backend/src/acex/models/aaa_model.py
@@ -0,0 +1,41 @@
+from sqlmodel import SQLModel, Field
+from typing import Any
+
+class aaaBaseClass(SQLModel):
+    name: str = None
+    
+class aaaBase(aaaBaseClass):
+    enable: bool = False
+    tacacs_servers: list = None
+    tacacs_all: dict = {
+        'name':'tacacs_vip',
+        'timeout':30,
+        'port':49
+    }
+
+class aaaAuthentication(aaaBaseClass):
+    method: list = None # Ex. ['TACACS_GROUP','LOCAL']
+
+class aaaAuthorizaion(aaaBaseClass):
+    method: list = None # Ex. ['TACACS_GROUP','LOCAL']
+
+class aaaAccounting(aaaBaseClass):
+    method: list = None # Ex. ['TACACS_GROUP','LOCAL']
+
+class aaaAccountingEvents(aaaBaseClass):
+    event: list = [
+        {
+        'event-type': 'command',
+        'config': {
+            'event-type': 'command',
+            'method': ['tacacs_group']
+            }
+        },
+        {
+        'event-type': 'system',
+        'config': {
+            'event-type': 'system',
+            'method': ['tacacs_group']
+            }
+        }
+    ]
