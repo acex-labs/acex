d55a6ab14e84dfb76bbbd8c43ff397dade7024c9
Jani Naamanka <jani.naamanka.ext@essity.com>
2025-11-12 12:39:00 +0100

    Changed name from NtpAttributes to NTPServerAttributes

--- backend/src/acex/configuration/components/system/ntp.py
+++ backend/src/acex/configuration/components/system/ntp.py
@@ -1,6 +1,6 @@
 from acex.configuration.components.base_component import ConfigComponent
-from acex.models.ntp_server import NtpAttributes
+from acex.models.ntp_server import NTPServerAttributes
 
 class NTPServer(ConfigComponent):
     type = "ntp_server"
-    model_cls = NtpAttributes
+    model_cls = NTPServerAttributes
--- backend/src/acex/models/ntp_server.py
+++ backend/src/acex/models/ntp_server.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Any
 
-class NtpAttributes(SQLModel):
+class NTPServerAttributes(SQLModel):
     name: str = None
     address: str = None
     prefer: bool = False
