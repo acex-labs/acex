cc726d010419c7ee4ba31a23808c3abaf4585543
Jani Naamanka <jani@naacon.se>
2025-11-12 14:17:11 +0100

    Changed so that variable and class names follow pascal/peb8 standard

--- docs/examples/example1/config_maps/system/ntp.py
+++ docs/examples/example1/config_maps/system/ntp.py
@@ -1,19 +1,27 @@
 from acex.config_map import ConfigMap, FilterAttribute
 <<<<<<< HEAD
+<<<<<<< HEAD
 from acex.configuration.components.system.ntp import NtpServer
 =======
 from acex.configuration.components.system.ntp import NTPServer
 >>>>>>> 4ccc9e1 (Add support for ntp)
+=======
+from acex.configuration.components.system.ntp import NtpServer
+>>>>>>> 3fccc02 (Changed so that variable and class names follow pascal/peb8 standard)
 
 
 class NtpConfig(ConfigMap):
     def compile(self, context):
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         ntp1 = NtpServer(
 =======
         ntp1 = NTPServer(
 >>>>>>> 4ccc9e1 (Add support for ntp)
+=======
+        ntp1 = NtpServer(
+>>>>>>> 3fccc02 (Changed so that variable and class names follow pascal/peb8 standard)
             name='se.pool.ntp.org',
             address='se.pool.ntp.org',
             prefer=True
@@ -21,10 +29,14 @@ class NtpConfig(ConfigMap):
         context.configuration.add(ntp1)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         ntp2 = NtpServer(
 =======
         ntp2 = NTPServer(
 >>>>>>> 4ccc9e1 (Add support for ntp)
+=======
+        ntp2 = NtpServer(
+>>>>>>> 3fccc02 (Changed so that variable and class names follow pascal/peb8 standard)
             name='fi.pool.ntp.org',
             address='fi.pool.ntp.org',
             prefer=True
