ddc02fec30350e3461d39cb587b2731b3985fa28
= <johan.lahti@acebit.se>
2025-11-14 13:35:37 +0100

    mr fix

--- backend/src/acex/models/ntp_server.py
+++ backend/src/acex/models/ntp_server.py
@@ -1,15 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Any
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 class NtpServerAttributes(SQLModel):
-=======
-class NtpAttributes(SQLModel):
->>>>>>> 4ccc9e1 (Add support for ntp)
-=======
-class NtpServerAttributes(SQLModel):
->>>>>>> 3fccc02 (Changed so that variable and class names follow pascal/peb8 standard)
     name: str = None
     address: str = None
     prefer: bool = False
