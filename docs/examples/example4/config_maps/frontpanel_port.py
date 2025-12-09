7dd9de54128e9552826629c8fda5a71377b5549c
Jani Naamanka <jani@naacon.se>
2025-12-09 13:08:44 +0100

    Reworked how the config map for frontpanels how it might look like depending on standard

--- docs/examples/example4/config_maps/frontpanel_port.py
+++ docs/examples/example4/config_maps/frontpanel_port.py
@@ -4,14 +4,24 @@ from acex.configuration.components.network_instances import L3Vrf
 
 
 interface_templates = {
-    "default": {'description': None, 'switchport': False, 'switchport_mode': None, 'enabled': True}, # Portar utan template = default?
+    "default": {'description': None, 'switchport': False, 'switchport_mode': None, 'enabled': False},
     "mgmt": {'description': 'LAN_Management', 'switchport': True, 'switchport_mode': 'access', 'switchport_untagged_vlan': 2, 'enabled': True},
     "print": {'description': 'Printers', 'switchport': True, 'switchport_mode': 'access', 'switchport_untagged_vlan': 61, 'enabled': True},
     "guest": {'description': 'GUEST', 'switchport': True, 'switchport_mode': 'access', 'switchport_untagged_vlan': 129, 'enabled': True},
     "iotwired": {'description': 'IoTWired', 'switchport': True, 'switchport_mode': 'access', 'switchport_untagged_vlan': 601, 'enabled': True},
 }
 
-interface_list = [    
+interface_list = [
+    {
+        'interface': 'Port-1/0/1',
+        'index': 8,
+        '_template': 'print'
+    },
+    {
+        'interface': 'Port-1/0/2',
+        'index': 9,
+        '_template': 'default'
+    },
     {
         'interface':'Port-1/0/25',
         'index': 32,
@@ -23,29 +33,19 @@ interface_list = [
         '_template': 'mgmt'
     },
     {
-        'interface':'Port-1/0/27',
-        'index': 34,
-        '_template': 'mgmt'
-    },
-    {
-        'interface':'Port-1/0/28',
-        'index': 35,
-        '_template': 'mgmt'
-    },
-    {
-        'interface':'Port-1/0/29',
-        'index': 36,
-        '_template': 'mgmt'
-    },
-    {
-        'interface':'Port-1/0/30',
-        'index': 37,
-        '_template': 'mgmt'
-    },
-    {
         'interface':'Port-1/0/31',
         'index': 38,
         '_template': 'mgmt'
+    },
+    {
+        'interface':'Port-1/0/48',
+        'index': 55,
+        'switchport': True,
+        'switchport_mode': 'trunk',
+        'native_vlan': 123,
+        'enabled': True,
+        'speed': 10000000,
+        'description': 'Uplink to Core'
     },
     {
         'interface':'Port-2/0/1',
@@ -64,79 +64,27 @@ interface_list = [
     }
 ]
 
+# Define each interface with a loop. This is just and example. If you want to keep it easy you can let the below logic be and only work
+# with the interface list and templates above.
 class Interfaces(ConfigMap):
     def compile(self, context):
-        # Specific ports
         for intf in interface_list:
 
             interface = FrontPanelPort(
-                name = intf["interface"],
-                index = intf["index"],
-                enabled = interface_templates.get(intf.get("_template", {}), {}).get("enabled") or intf.get("enabled", False),
-                description = interface_templates.get(intf.get("_template", {}), {}).get("description"),
-                switchport_mode = interface_templates.get(intf.get("_template", {}), {}).get("switchport_mode") or intf.get("switchport_mode"),
-                switchport = interface_templates.get(intf.get("_template", {}), {}).get("switchport") or intf.get("switchport"),
-                access_vlan = interface_templates.get(intf.get("_template", {}), {}).get("access_vlan") or intf.get("access_vlan"),
-                native_vlan = interface_templates.get(intf.get("_template", {}), {}).get("native_vlan") or intf.get("native_vlan")
+                name = intf.get("interface"),
+                index = intf.get("index"),
+                trunk_allowed_vlans = intf.get('trunk_allowed_vlans') if intf.get('trunk_allowed_vlans') else None,
+                enabled = interface_templates.get(intf.get("_template"), {}).get("enabled") or intf.get("enabled"),
+                description = interface_templates.get(intf.get("_template"), {}).get("description") or intf.get("description"),
+                switchport_mode = interface_templates.get(intf.get("_template"), {}).get("switchport_mode") or intf.get("switchport_mode"),
+                switchport = interface_templates.get(intf.get("_template"), {}).get("switchport") or intf.get("switchport"),
+                access_vlan = interface_templates.get(intf.get("_template"), {}).get("access_vlan") or intf.get("access_vlan"),
+                native_vlan = interface_templates.get(intf.get("_template"), {}).get("native_vlan") or intf.get("native_vlan")
                 )
             context.configuration.add(interface)
 
-        # Standard ports below. Only change if a port is removed from the loop.
-        # Standard ports for PCs
-        for i in range(0,23): # stack 1?
-            # 1/0/1 - 1/0/24
-            accessport = FrontPanelPort(
-                name=f'port-{i}',
-                index=i,
-                description='accessport',
-                enabled=True,
-                switchport_mode='access',
-                switchport=True,
-                native_vlan=10
-            )
-
-            context.configuration.add(accessport)
-        
-        for i in range(32,47):  # stack 1?
-            # 1/0/33 - 1/0/48 
-            accessport = FrontPanelPort(
-                name=f'port-{i}',
-                index=i,
-                description='accessport',
-                enabled=True,
-                switchport_mode='access',
-                switchport=True,
-                native_vlan=10
-            )
-
-            context.configuration.add(accessport)
-
-        trunk1 = FrontPanelPort(
-            name='trunk-1',
-            # 1/1/3
-            index=51,
-            description='To core',
-            switchport_mode='trunk',
-            switchport=True,
-            native_vlan=123,
-            enabled=True
-        )
-
-        context.configuration.add(trunk1)
-
-        trunk2 = FrontPanelPort(
-            name='trunk-2',
-            # 4/1/3
-            index=223, # ???
-            description='To core',
-            switchport_mode='trunk',
-            switchport=True,
-            native_vlan=123,
-            enabled=True
-        )
-
-        context.configuration.add(trunk2)
-
+# Define a mgmt interface. A vrf is created and assigned to the mgmt interface. If you do not want to have a vrf binding on the
+# mgmt interface you can skip the network_instance parameter when creating the ManagementPort instance.
 class ManagementInterface(ConfigMap):
     def compile(self, context):
         vrf = L3Vrf(
