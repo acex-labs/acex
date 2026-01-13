84ad94e5cad23534b62121b11a098494489afe71
Jani Naamanka <jani@naacon.se>
2026-01-13 14:38:49 +0100

    Updated spanning tree model with more options according to openconfig standard

--- backend/src/acex/models/spanning_tree.py
+++ backend/src/acex/models/spanning_tree.py
@@ -4,16 +4,18 @@ from .composed_configuration import Reference, ReferenceFrom, ReferenceTo
 from enum import Enum
 from typing import Optional, Dict
 
-class SpanningTreeConfig(BaseModel):
-    mode: Optional[AttributeValue[SpanningTreeMode]] = None
-    bridge_priority: Optional[AttributeValue[int]] = None
+class SpanningTreeModeConfig(BaseModel):
     hello_time: Optional[AttributeValue[int]] = None
     max_age: Optional[AttributeValue[int]] = None
     forward_delay: Optional[AttributeValue[int]] = None
+    bridge_priority: Optional[AttributeValue[int]] = None
+
+class SpanningTreeGlobal(SpanningTreeModeConfig):
+    mode: Optional[AttributeValue[SpanningTreeMode]] = "RSTP"
     bpdu_filter: Optional[AttributeValue[bool]] = False # Disabled by default
     bpdu_guard: Optional[AttributeValue[bool]] = False # Disabled by default
     loop_guard: Optional[AttributeValue[bool]] = False # Disabled by default
-    interfaces: Optional[Dict[str, Reference]] = {}
+    #interfaces: Optional[Dict[str, Reference]] = {}
 
 class SpanningTreeMode(str, Enum): 
     RSTP = "RSTP"
@@ -21,13 +23,17 @@ class SpanningTreeMode(str, Enum):
     PVST = "PVST"
     PVST_PLUS = "PVST_PLUS"
 
-#class SpanningTreeBpduFilter(str, Enum):
-#    ENABLED = "ENABLED"
-#    DISABLED = "DISABLED"  
-#
-#class SpanningTreeBpduGuard(str, Enum):
-#    ENABLED = "ENABLED"
-#    DISABLED = "DISABLED"
+class SpanningTreeRSTP(SpanningTreeModeConfig): 
+    interfaces: Optional[Dict[str, Reference]] = {}
+
+class SpanningTreeMSTP(SpanningTreeModeConfig):
+    interfaces: Optional[Dict[str, Reference]] = {}
+    mst_instances: Optional[Dict[str, Reference]] = {}
+    vlan: Optional[Dict[str, Reference]] = {}
+
+class SpanningTreeRapidPVST(SpanningTreeModeConfig):
+    interfaces: Optional[Dict[str, Reference]] = {}
+    vlan: Optional[Dict[str, Reference]] = {}
 
 # How use this per interface?
 class SpanningTreeInterfaceConfig(BaseModel):
