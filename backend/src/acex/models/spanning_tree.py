abe6011ba9379f9939b8df719795eaf7f6721878
Jani Naamanka <jani@naacon.se>
2026-01-19 11:42:43 +0100

    Updated spanning tree model with more options for modes and global settings

--- backend/src/acex/models/spanning_tree.py
+++ backend/src/acex/models/spanning_tree.py
@@ -9,12 +9,15 @@ class SpanningTreeModeConfig(BaseModel):
     max_age: Optional[AttributeValue[int]] = None
     forward_delay: Optional[AttributeValue[int]] = None
     bridge_priority: Optional[AttributeValue[int]] = None
+    hold_count: Optional[AttributeValue[int]] = None # Range 1..10
 
-class SpanningTreeGlobal(SpanningTreeModeConfig):
+class SpanningTreeGlobal(BaseModel):
     mode: Optional[AttributeValue[str]] = "RSTP"
     bpdu_filter: Optional[AttributeValue[bool]] = False # Disabled by default
     bpdu_guard: Optional[AttributeValue[bool]] = False # Disabled by default
     loop_guard: Optional[AttributeValue[bool]] = False # Disabled by default
+    portfast: Optional[AttributeValue[bool]] = False # Disabled by default. Global setting for access ports.
+    bridge_assurance: Optional[AttributeValue[bool]] = False # Disabled by default. Only supported by MST and PVRST+
     #interfaces: Optional[Dict[str, Reference]] = {}
 
 class SpanningTreeMode(str, Enum): 
@@ -23,26 +26,41 @@ class SpanningTreeMode(str, Enum):
     PVST = "PVST"
     PVST_PLUS = "PVST_PLUS"
 
-class SpanningTreeRSTP(SpanningTreeModeConfig): ...
+class SpanningTreeRSTP(SpanningTreeModeConfig):
+    name = "RSTP"
 #    interfaces: Optional[Dict[str, Reference]] = {}
 
-class SpanningTreeMSTP(SpanningTreeModeConfig): ...
+class MstInstanceConfig(BaseModel):
+    instance_id: AttributeValue[int] # range: 1..4094
+    #name: Optional[AttributeValue[str]] = None
+    vlan_range: Optional[AttributeValue[str]] = None # List of VLANs mapped to the MST instance
+    hello_time: Optional[AttributeValue[int]] = None
+    max_age: Optional[AttributeValue[int]] = None
+    forward_delay: Optional[AttributeValue[int]] = None
+    bridge_priority: Optional[AttributeValue[int]] = None
+
+class SpanningTreeMSTP(SpanningTreeModeConfig):
+    name = "MSTP"
+    revision: Optional[AttributeValue[int]] = None
+    max_hop: Optional[AttributeValue[int]] = None # Range 1..255
+    mst_instances: Optional[Dict[str, MstInstanceConfig]] = {}
+
 #    interfaces: Optional[Dict[str, Reference]] = {}
 #    mst_instances: Optional[Dict[str, Reference]] = {}
 #    vlan: Optional[Dict[str, Reference]] = {}
 
-class SpanningTreeRapidPVST(SpanningTreeModeConfig): ...
+class SpanningTreeRapidPVST(SpanningTreeModeConfig):
+    name = "PVST_PLUS"
 #    interfaces: Optional[Dict[str, Reference]] = {}
 #    vlan: Optional[Dict[str, Reference]] = {}
 
-# How use this per interface?
-class SpanningTreeInterfaceConfig(BaseModel):
-    port_priority: Optional[AttributeValue[int]] = None
-    cost: Optional[AttributeValue[int]] = None
-    edge_port: Optional[AttributeValue[bool]] = False # Disabled by default
-    bpdu_filter: Optional[AttributeValue[bool]] = False # Disabled by default
-    bpdu_guard: Optional[AttributeValue[bool]] = False # Disabled by default
-    loop_guard: Optional[AttributeValue[bool]] = False # Disabled by default
-    root_guard: Optional[AttributeValue[bool]] = False # Disabled by default
-    portfast: Optional[AttributeValue[bool]] = False # Disabled by default
-    link_type: Optional[AttributeValue[str]] = None  # e.g., "point-to-point", "shared"
+#class SpanningTreeInterfaceConfig(BaseModel):
+#    port_priority: Optional[AttributeValue[int]] = None
+#    cost: Optional[AttributeValue[int]] = None
+#    edge_port: Optional[AttributeValue[bool]] = False # Disabled by default
+#    bpdu_filter: Optional[AttributeValue[bool]] = False # Disabled by default
+#    bpdu_guard: Optional[AttributeValue[bool]] = False # Disabled by default
+#    loop_guard: Optional[AttributeValue[bool]] = False # Disabled by default
+#    root_guard: Optional[AttributeValue[bool]] = False # Disabled by default
+#    portfast: Optional[AttributeValue[bool]] = False # Disabled by default
+#    stp_link_type: Optional[Literal["point-to-point", "shared"]] = None  # e.g., "point-to-point", "shared"
