7ece33245537cf1137233677bef8d12d8b639664
Jani Naamanka <jani@naacon.se>
2026-01-20 15:58:36 +0100

    Reworked STP config map as models and config changed

--- docs/examples/example9/config_maps/spanning_tree.py
+++ docs/examples/example9/config_maps/spanning_tree.py
@@ -1,5 +1,5 @@
 from acex.config_map import ConfigMap, FilterAttribute
-from acex.configuration.components.spanning_tree import SpanningTreeGlobal, SpanningTreeRSTP, SpanningTreeMSTP, SpanningTreeRapidPVST
+from acex.configuration.components.spanning_tree import SpanningTreeGlobal, SpanningTreeRSTP, SpanningTreeMSTP, SpanningTreeMstpInstance
 
 class STPConfig(ConfigMap):
     def compile(self, context):
@@ -9,13 +9,61 @@ class STPConfig(ConfigMap):
         )
         context.configuration.add(stp_global)
 
-class RapidPVSTConfig(ConfigMap):
+class RSTPConfig(ConfigMap):
     def compile(self, context):
-        rapid_pvst = SpanningTreeRapidPVST(
-            vlan=[10, 20, 30],
-            bridge_priority=4096
+        rstp = SpanningTreeRSTP(
+            bridge_priority = 32768,
+            hello_time = 2,
+            max_age = 20,
+            forward_delay = 15,
+            hold_count = 3
         )
-        context.configuration.add(rapid_pvst)
+        context.configuration.add(rstp)
+
+class MSTConfig(ConfigMap):
+    def compile(self, context):
+        mstp_global = SpanningTreeMSTP(
+            revision = 1,
+            bridge_priority = 4096,
+            hello_time = 2,
+            max_age = 20,
+            forward_delay = 15
+        )
+        context.configuration.add(mstp_global)
+
+        mstp_instance1 = SpanningTreeMstpInstance(
+            name = "MST1",
+            instance_id = 1,
+            vlan = [10,20,30],
+            bridge_priority = 8192
+        )
+        context.configuration.add(mstp_instance1)
+
+        mstp_instance2 = SpanningTreeMstpInstance(
+            name = "MST2",
+            instance_id = 2,
+            vlan = [40,50,60],
+            bridge_priority = 8192
+        )
+        context.configuration.add(mstp_instance2)
+
+
+#class RapidPVSTConfig(ConfigMap):
+#    def compile(self, context):
+#        rapid_pvst = SpanningTreeRapidPVST(
+#            vlan=10,
+#            bridge_priority=4096
+#        )
+#        context.configuration.add(rapid_pvst)
 
 stpconfig = STPConfig()
 stpconfig.filters = FilterAttribute("site").eq("/.*/")
+
+rapidstpconfig = RSTPConfig()
+rapidstpconfig.filters = FilterAttribute("site").eq("/.*/")
+
+mstconfig = MSTConfig()
+mstconfig.filters = FilterAttribute("site").eq("/.*/")
+
+#rapidpvstpconfig = RapidPVSTConfig()
+#rapidpvstpconfig.filters = FilterAttribute("site").eq("/.*/")
