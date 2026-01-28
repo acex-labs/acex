98c37422056e2f51af734605d9b9a66fb1b955ba
Jani Naamanka <jani@naacon.se>
2026-01-28 09:35:08 +0100

    Updated config component for snmp. Removed stuff not needed and changed import as code is moved to composed_configuration

--- backend/src/acex/configuration/components/system/snmp.py
+++ backend/src/acex/configuration/components/system/snmp.py
@@ -2,7 +2,7 @@ from acex.configuration.components.base_component import ConfigComponent
 from acex.models.composed_configuration import ReferenceTo, ReferenceFrom
 from acex.configuration.components.interfaces import Interface
 from acex.configuration.components.network_instances import NetworkInstance
-from acex.models.snmp import (
+from acex.models.composed_configuration import (
     SnmpConfig, 
     SnmpUser as SnmpUserAttributes,
     SnmpServer as SnmpServerAttributes,
@@ -22,9 +22,6 @@ class SnmpServer(ConfigComponent):
     model_cls = SnmpServerAttributes
 
     def _add_vrf(self):
-        #if self.kwargs.get('network_instance'):
-        #    network_instance = self.kwargs.pop("network_instance")
-        #    self.kwargs["network_instance"] = ReferenceFrom(pointer=f"network_instances.{network_instance.name}")
         if self.kwargs.get('network_instance'):
             ni = self.kwargs.pop("network_instance")
             if isinstance(ni, type(None)):
@@ -55,5 +52,3 @@ class SnmpServer(ConfigComponent):
 class SnmpTrap(ConfigComponent):
     type = "snmp_trap"
     model_cls = TrapEvent
-
-# Traps?
