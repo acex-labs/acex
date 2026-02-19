330bfb05b9a343ac013f8777f2552710e5bbda86
Jani Naamanka <jani@naacon.se>
2026-02-19 11:42:13 +0100

    add app.py

--- docs/examples/example12_banner/app.py
+++ docs/examples/example12_banner/app.py
@@ -4,37 +4,39 @@ from acex.plugins.integrations import Sqlite, Netbox
 from acex.database import Connection
 import os
 
-
 # Database (Postgres)
 db = Connection(
-    dbname="mydb17",
-    user="admin",
-    password="admin123",
+    dbname="ace",
+    user="postgres",
+    password="",
     host="localhost",
     backend="postgresql"
 )
 
-#db = Connection(
-#    dbname="automation_framework",
-#    user="acex_api",
-#    password="",
-#    host="10.116.132.85",
-#    backend="postgresql"
-#)
 
 # # External datasources
 netbox = Netbox(
-    url="https://netbox.ngninfra.net/",
+    url="https://netbox.example.net/",
     token=os.getenv("NETBOX_TOKEN"),
     verify_ssl=False,
 )
 
 ae = AutomationEngine(
-    db_connection=db
+    db_connection=db,
+    # assets_plugin=netbox,
+    # logical_nodes_plugin=netbox,
 )
 
 ae.add_integration("ipam", netbox)
 ae.add_configmap_dir("config_maps")
+
+# AI OPS
+ae.ai_ops(
+    enabled=True,
+    base_url=os.getenv("ACEX_AI_API_BASEURL"),
+    api_key=os.getenv("ACEX_AI_API_KEY"),
+    mcp_server_url=os.getenv("ACEX_MCP_URL")
+)
 
 # CORS
 ae.add_cors_allowed_origin("*")
@@ -50,3 +52,4 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=80,
     )
+    
