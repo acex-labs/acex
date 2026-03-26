2cb5654e7a0dfab37479dc93d9ce787e7ab869af
johan-lahti <59826785+johan-lahti@users.noreply.github.com>
2026-03-26 15:30:17 +0100

    feat: left-joined logical_node.hostname and site to node-list (#87)
    Co-authored-by: = <johan.lahti@acebit.se>

--- backend/src/acex/models/node.py
+++ backend/src/acex/models/node.py
@@ -1,9 +1,12 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional, Dict
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, Dict, TYPE_CHECKING
 from enum import Enum
 
 from acex.models import LogicalNodeResponse
 from acex.models import AssetResponse
+
+if TYPE_CHECKING:
+    from acex.models.logical_node import LogicalNode
 
 
 class AssetRefType(str, Enum):
@@ -14,10 +17,18 @@ class AssetRefType(str, Enum):
 class NodeBase(SQLModel):
     asset_ref_id: int
     asset_ref_type: AssetRefType = AssetRefType.asset
-    logical_node_id: int
+    logical_node_id: int = Field(foreign_key="logicalnode.id")
 
 class Node(NodeBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    logical_node: Optional["LogicalNode"] = Relationship(
+        sa_relationship_kwargs={"lazy": "noload"}
+    )
+
+class NodeListResponse(NodeBase):
+    id: int
+    hostname: Optional[str] = None
+    site: Optional[str] = None
 
 class NodeResponse(NodeBase):
     asset: AssetResponse
