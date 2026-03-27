75e41fcc0faaef8dac863c617ee20496773f6fe6
Jani Naamanka <jani@naacon.se>
2026-03-27 16:23:14 +0100

    Rework SNMP in parser - separated each section into its own function to have smaller and easier to read textfsm, also easier to work through data - separate textfsm for each section of snmp - clean up code and fix issues

--- drivers/cisco_ios_cli/src/acex_driver_cisco_ioscli/parser.py
+++ drivers/cisco_ios_cli/src/acex_driver_cisco_ioscli/parser.py
@@ -16,6 +16,7 @@ from acex_devkit.models.composed_configuration import (
     SnmpServer,
     TrapEvent,
     SnmpView,
+    SnmpSecurityLevel,
     ReferenceTo,
 )
 from ntc_templates.parse import parse_output
@@ -98,6 +99,11 @@ class CiscoIOSCLIParser:
         self.parse_ntp()
         self.parse_ssh()
         self.parse_snmp()
+        self.parse_snmp_servers()
+        self.parse_snmp_views()
+        self.parse_snmp_communities()
+        self.parse_snmp_users()
+
 
         return self._parsed_config
 
@@ -332,7 +338,167 @@ class CiscoIOSCLIParser:
     def load_running_config(self, filepath: str) -> str:
         with open(filepath, 'r') as f:
             return f.read()
-    
+
+    def parse_snmp_traps(self) -> None:
+        """Parse SNMP trap configuration."""
+        command = "show running snmp traps"
+
+        parsed_data = parse_output(
+            platform=self.platform,
+            template_dir=self.custom_templates_dir,
+            command=command,
+            data=self.running_config
+        )
+
+        snmp_traps_dict = {}
+        for entry in parsed_data:
+            snmp_trap_values_dict = {}
+            # Every trap that is allowed is specified in TrapEvent model, so we loop through all traps and check if they are enabled, if they are enabled we add them to the trap_events list in the SnmpConfig
+            # Below code needs to be fixed and handled correctly as the config that Cisco gives does not match exactly how the traps are defined in the model.
+            if entry.get('traps'):
+                for i, trap in enumerate(entry.get('traps')):
+                    snmp_trap_values_dict['name'] = f"trap_{i}"
+                    snmp_trap_values_dict['event_name'] = trap
+                    snmp_traps_dict[entry.get('event_name')] = self.removekey(TrapEvent(**snmp_trap_values_dict), 'metadata')
+
+        self.parsed_config.system.snmp.trap_events = snmp_traps_dict    
+
+    def parse_snmp_views(self) -> None:
+        """Parse SNMP view configuration."""
+        command = "show running snmp views"
+
+        parsed_data = parse_output(
+            platform=self.platform,
+            template_dir=self.custom_templates_dir,
+            command=command,
+            data=self.running_config
+        )
+
+        snmp_views_dict = {}
+        for i, entry in enumerate(parsed_data):
+            snmp_view_values_dict = {}
+            if entry.get("view_name"):
+                snmp_view_values_dict['name'] = f"{entry.get('view_name')}_{i}" if entry.get("view_name") else None
+                snmp_view_values_dict['oid'] = entry.get("view_oid") if entry.get("view_oid") else None
+                snmp_view_values_dict['included'] = True if 'included' in entry.get("view_status") else False
+                snmp_views_dict[f"{entry.get('view_name')}_{i}"] = self.removekey(SnmpView(**snmp_view_values_dict), 'metadata')
+        
+        self.parsed_config.system.snmp.views = snmp_views_dict
+
+    def parse_snmp_servers(self) -> None:
+        """Parse SNMP server configuration."""
+        command = "show running snmp servers"
+
+        parsed_data = parse_output(
+            platform=self.platform,
+            template_dir=self.custom_templates_dir,
+            command=command,
+            data=self.running_config
+        )
+
+        snmp_servers_dict = {}
+        for entry in parsed_data:
+            snmp_server_values_dict = {}
+            snmp_server_values_dict['address'] = entry.get("host") 
+            if entry.get("host"):
+                snmp_server_values_dict['address'] = str(entry.get("host"))
+                snmp_server_values_dict['enabled'] = True
+            else:
+                snmp_server_values_dict['address'] = None
+                snmp_server_values_dict['enabled'] = False
+            snmp_server_values_dict['port'] = int(entry.get("server_port")) if entry.get("server_port") else None
+            if entry.get("version"):
+                version = 'v3' if entry.get("version") == '3' else 'v2c' if entry.get("version") == '2c' else None
+                snmp_server_values_dict['version'] = version
+            snmp_server_values_dict['community'] = entry.get("community_string") if entry.get("community_string") else None
+            snmp_server_values_dict['username'] = entry.get("server_user") if entry.get("server_user") else None
+            snmp_server_values_dict['security_level'] = entry.get("server_security_level") if entry.get("server_security_level") else None
+            if entry.get("source_interface"):
+                for intf_name, intf in self.parsed_config.interfaces.items():
+                    intf_type = intf.get('type') if isinstance(intf, dict) else getattr(intf, 'type', None)
+                    intf_vlan_id = intf['vlan_id'].get('value') if isinstance(intf, dict) and intf.get('vlan_id') else getattr(intf, 'vlan_id', None)
+                    if intf_type == 'l3ipvlan' and intf_vlan_id == int(entry.get("source_interface").replace('Vlan','')):
+                        intf_ref = ReferenceTo(pointer=f"interfaces.{intf_name}")
+                        snmp_server_values_dict['source_interface'] = intf_ref
+                        break
+                    else:
+                        snmp_server_values_dict['source_interface'] = None
+            snmp_server_values_dict['network_instance'] = entry.get("vrf") if entry.get("vrf") else None
+
+            snmp_servers_dict[entry.get('host')] = self.removekey(SnmpServer(**snmp_server_values_dict), 'metadata')
+
+        self.parsed_config.system.snmp.trap_servers = snmp_servers_dict
+
+    def parse_snmp_users(self) -> None:
+        """Parse SNMP user configuration."""
+        command = "show running snmp users"
+
+        parsed_data = parse_output(
+            platform=self.platform,
+            template_dir=self.custom_templates_dir,
+            command=command,
+            data=self.running_config
+        )
+
+        snmp_users_dict = {}
+        for i, entry in enumerate(parsed_data):
+            snmp_user_values_dict = {}
+            snmp_user_values_dict['name'] = f'user_{i}'
+            snmp_user_values_dict['username'] = entry.get("user") if entry.get("user") else None
+            # Security levels
+            #   auth    group using the authNoPriv Security Level
+            #   noauth  group using the noAuthNoPriv Security Level
+            #   priv    group using SNMPv3 authPriv security level
+            if entry.get("security_level") == "auth":
+                snmp_user_values_dict['security_level'] = "AUTH_NO_PRIV"
+            elif entry.get("security_level") == "noauth":
+                snmp_user_values_dict['security_level'] = "NO_AUTH_NO_PRIV"
+            elif entry.get("security_level") == "priv":
+                snmp_user_values_dict['security_level'] = "AUTH_PRIV"
+            else:
+                None
+            snmp_user_values_dict['auth_protocol'] = entry.get("auth_protocol") if entry.get("auth_protocol") else None
+            snmp_user_values_dict['auth_password'] = entry.get("auth_password") if entry.get("auth_password") else None
+            snmp_user_values_dict['priv_protocol'] = entry.get("priv_protocol") if entry.get("priv_protocol") else None
+            snmp_user_values_dict['priv_password'] = entry.get("priv_password") if entry.get("priv_password") else None
+
+            snmp_users_dict[f'user_{i}'] = self.removekey(SnmpUser(**snmp_user_values_dict), 'metadata')
+
+        self.parsed_config.system.snmp.users = snmp_users_dict
+
+    def parse_snmp_communities(self) -> None:
+        """Parse SNMP community configuration."""
+        command = "show running snmp communities"
+
+        parsed_data = parse_output(
+            platform=self.platform,
+            template_dir=self.custom_templates_dir,
+            command=command,
+            data=self.running_config
+        )
+
+        snmp_communities_dict = {}
+        for i, entry in enumerate(parsed_data):
+            snmp_community_values_dict = {}
+            snmp_community_values_dict['name'] = f'community_{i}'
+            snmp_community_values_dict['community'] = entry.get("community_string") if entry.get("community_string") else None
+            snmp_community_values_dict['access'] = entry.get("access") if entry.get("access") else None
+            # Should be a reference to an existing SnmpView
+            #snmp_community_values_dict['view'] = entry.get("view") if entry.get("view") else None
+            snmp_community_values_dict['ipv4_acl'] = entry.get("ipv4_acl") if entry.get("ipv4_acl") else None
+            snmp_community_values_dict['ipv6_acl'] = entry.get("ipv6_acl") if entry.get("ipv6_acl") else None
+            if entry.get("source_interface"):
+                for intf_name, intf in self.parsed_config.interfaces.items():
+                    intf_type = intf.get('type') if isinstance(intf, dict) else getattr(intf, 'type', None)
+                    intf_vlan_id = intf['vlan_id'].get('value') if isinstance(intf, dict) and intf.get('vlan_id') else getattr(intf, 'vlan_id', None)
+                    if intf_type == 'l3ipvlan' and intf_vlan_id == int(entry.get("source_interface").replace('Vlan','')):
+                        intf_ref = ReferenceTo(pointer=f"interfaces.{intf_name}")
+                        break
+                snmp_community_values_dict['source_interface'] = intf_ref
+
+            snmp_communities_dict[f'community_{i}'] = self.removekey(SnmpCommunity(**snmp_community_values_dict), 'metadata')
+        self.parsed_config.system.snmp.communities = snmp_communities_dict
+
     def parse_snmp(self) -> None:
         """Parse SNMP configuration."""
         command = "show running snmp"
@@ -345,21 +511,10 @@ class CiscoIOSCLIParser:
             #data=self.load_running_config("/Users/jani/scripts/acex/docs/examples/test_run/sample_running.txt") # Using this for testing with a static config file, replace with self.running_config for actual use
         )
 
-        #print('Running: ', self.running_config)
-        print('Parsed SNMP data: ', parsed_data)
-
         # SNMP parsing logic would go here, similar to NTP and SSH parsing
-        snmp_dict = {}
         snmp_config_values_dict = {}
-        snmp_communities_dict = {}
-        snmp_users_dict = {}
-        snmp_views_dict = {}
-        snmp_servers_dict = {}
-        snmp_traps_dict = {}
 
         for entry in parsed_data:
-            print('SNMP entry: ', entry)
-            # Config values
             if entry.get('host'):
                 snmp_config_values_dict['enabled'] = True
             snmp_config_values_dict['location'] = entry.get("location") if entry.get("location") else None
@@ -367,82 +522,5 @@ class CiscoIOSCLIParser:
             snmp_config_values_dict['engine_id'] = entry.get("engine_id") if entry.get("engine_id") else None
 
             snmp_config = self.removekey(SnmpConfig(**snmp_config_values_dict), 'metadata')
-            
-            # Community values
-            snmp_community_values_dict = {}
-            snmp_community_values_dict['name'] = 'test'
-            snmp_community_values_dict['community'] = entry.get("community_string") if entry.get("community_string") else None
-            snmp_community_values_dict['access'] = entry.get("access") if entry.get("access") else None
-            snmp_community_values_dict['view'] = entry.get("view") if entry.get("view") else None
-            snmp_community_values_dict['ipv4_acl'] = entry.get("ipv4_acl") if entry.get("ipv4_acl") else None
-            snmp_community_values_dict['ipv6_acl'] = entry.get("ipv6_acl") if entry.get("ipv6_acl") else None
-            if entry.get("source_interface"):
-                for intf_name, intf in self.parsed_config.interfaces.items():
-                    intf_type = intf.get('type') if isinstance(intf, dict) else getattr(intf, 'type', None)
-                    intf_vlan_id = intf.get('vlan_id') if isinstance(intf, dict) else getattr(intf, 'vlan_id', None)
-                    if intf_type == 'l3ipvlan' and intf_vlan_id == int(entry.get("source_interface").replace('Vlan','')):
-                        intf_ref = ReferenceTo(pointer=f"interfaces.{intf_name}")
-                        break
-                entry['source_interface'] = intf_ref
-                snmp_community_values_dict['source_interface'] = intf_ref
-            
-            snmp_communities_dict['test'] = self.removekey(SnmpCommunity(**snmp_community_values_dict), 'metadata')
 
-            # User values
-            snmp_user_values_dict = {}
-            snmp_user_values_dict['user'] = entry.get("user") if entry.get("user") else None
-            snmp_user_values_dict['security_level'] = entry.get("security_level") if entry.get("security_level") else None
-            snmp_user_values_dict['auth_protocol'] = entry.get("auth_protocol") if entry.get("auth_protocol") else None
-            snmp_user_values_dict['auth_password'] = entry.get("auth_password") if entry.get("auth_password") else None
-            snmp_user_values_dict['priv_protocol'] = entry.get("priv_protocol") if entry.get("priv_protocol") else None
-            snmp_user_values_dict['priv_password'] = entry.get("priv_password") if entry.get("priv_password") else None
-
-            #snmp_users_dict[entry.get('user')] = self.removekey(SnmpUser(**snmp_user_values_dict), 'metadata')
-
-            # View values
-            snmp_view_values_dict = {}
-            snmp_view_values_dict['name'] = entry.get("view_name") if entry.get("view_name") else None
-            snmp_view_values_dict['oid'] = entry.get("view_oid") if entry.get("view_oid") else None
-            snmp_view_values_dict['included'] = True if entry.get("view_included") else False
-
-            #snmp_views_dict[entry.get('view_name')] = self.removekey(SnmpView(**snmp_view_values_dict), 'metadata')
-
-            # Server values
-            snmp_server_values_dict = {}
-            snmp_server_values_dict['address'] = entry.get("host") 
-            if entry.get("host"):
-                snmp_server_values_dict['address'] = str(entry.get("host"))
-                snmp_server_values_dict['enabled'] = True
-            else:
-                snmp_server_values_dict['address'] = None
-                snmp_server_values_dict['enabled'] = False
-            snmp_server_values_dict['port'] = int(entry.get("server_port")) if entry.get("server_port") else None
-            snmp_server_values_dict['enabled'] = True if entry.get("server_enabled") else False
-            if entry.get("version"):
-                version = 'v3' if entry.get("version") == '3' else 'v2c' if entry.get("version") == '2c' else None
-                snmp_server_values_dict['version'] = version
-            #snmp_server_values_dict['version'] = entry.get("version") if entry.get("version") else None
-            snmp_server_values_dict['community'] = entry.get("community_string") if entry.get("community_string") else None
-            snmp_server_values_dict['username'] = entry.get("server_user") if entry.get("server_user") else None
-            snmp_server_values_dict['security_level'] = entry.get("server_security_level") if entry.get("server_security_level") else None
-            snmp_server_values_dict['source_interface'] = entry.get("source_interface") if entry.get("source_interface") else None
-            snmp_server_values_dict['network_instance'] = entry.get("vrf") if entry.get("vrf") else None
-
-            snmp_servers_dict[entry.get('host')] = self.removekey(SnmpServer(**snmp_server_values_dict), 'metadata')
-
-            # Trap events
-            snmp_trap_values_dict = {}
-            snmp_trap_values_dict['name'] = entry.get("trap_event_name") if entry.get("trap_event_name") else None
-            snmp_trap_values_dict['event'] = entry.get("trap_event") if entry.get("trap_event") else None
-
-            #snmp_traps_dict[entry.get('trap_event_name')] = self.removekey(TrapEvent(**snmp_trap_values_dict), 'metadata')
-
-
-        snmp_dict['config'] = snmp_config
-        snmp_dict['communities'] = snmp_communities_dict
-        #snmp_dict['users'] = snmp_users_dict
-        #snmp_dict['views'] = snmp_views_dict
-        snmp_dict['trap_events'] = snmp_traps_dict
-        snmp_dict['trap_servers'] = snmp_servers_dict
-#
-        self.parsed_config.system.snmp = snmp_dict
+        self.parsed_config.system.snmp.config = snmp_config
