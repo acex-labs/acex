c7b99613252bf55901b71797c8f2785562bb2a1a
Jani Naamanka <jani@naacon.se>
2026-04-15 10:29:10 +0200

    Support for cdp/lldp on interface and example update - add support for enabling or disabling cdp or lldp on an interface - updated config map in example2 with more values that can be defined

--- docs/examples/example2/config_maps/physical.py
+++ docs/examples/example2/config_maps/physical.py
@@ -12,9 +12,33 @@ class PhysicalPort(ConfigMap):
             name="if0",
             speed=1000000,
             description="Switchport1",
-            switchport = True
+            switchport = True,
+            switchport_mode = "access",
+            access_vlan = 10,
+            mtu = 1500,
+            negotiation = True,
+            lldp_enabled = False,
+            cdp_enabled = False
         )
         context.configuration.add(if0)
+
+        if1 = Physical(
+            index=1,
+            module_index=0,
+            stack_index=0,
+            name="if1",
+            speed=1000000,
+            description="Switchport2",
+            switchport = True,
+            switchport_mode = "trunk",
+            trunk_allowed_vlans = [10,20],
+            native_vlan = 123,
+            mtu = 1500,
+            negotiation = True,
+            lldp_enabled = True,
+            cdp_enabled = True
+        )
+        context.configuration.add(if1)
 
 intf = PhysicalPort()
 intf.filters = FilterAttribute("hostname").eq("/.*/")
