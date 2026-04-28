6208c6d17f56578ddb95464cea228ff29d8dc567
Jani Naamanka <jani@naacon.se>
2026-04-28 20:17:55 +0200

    Fixes to Netflow (#139)
    - updated variable types and added references to exporter and record in collector
    - added logic to netflow config components where needed for references
    - updated configmap with new examples according to changes
    - upped version in devkit and backend

--- backend/src/acex/configuration/components/sampling/netflow.py
+++ backend/src/acex/configuration/components/sampling/netflow.py
@@ -1,5 +1,6 @@
 from acex.configuration.components.base_component import ConfigComponent
 from acex_devkit.models.composed_configuration import (
+    Interface,
     NetflowCollectorAttributes,
     NetflowExporterAttributes,
     NetflowRecordAttributes,
@@ -7,6 +8,7 @@ from acex_devkit.models.composed_configuration import (
     NetflowRecordIpv4Match as NetflowRecordIpv4MatchAttributes,
     NetflowExporterOptions as NetflowExporterOptionsAttributes,
     ReferenceFrom,
+    ReferenceTo,
 )
 
 class NetflowGlobalConfig(ConfigComponent):
@@ -16,6 +18,16 @@ class NetflowGlobalConfig(ConfigComponent):
 class NetflowCollector(ConfigComponent): 
     type = "NetflowCollector"
     model_cls = NetflowCollectorAttributes
+
+    def pre_init(self):
+        # Ensure that referenced components are added to the configuration before this component is added
+        if self.kwargs.get("netflow_record") is not None:
+            netflow_record = self.kwargs.pop("netflow_record")
+            self.kwargs["netflow_record"] = netflow_record.name
+
+        if self.kwargs.get("netflow_exporter") is not None:
+            netflow_exporter = self.kwargs.pop("netflow_exporter")
+            self.kwargs["netflow_exporter"] = netflow_exporter.name
 
 class NetflowExporter(ConfigComponent): 
     type = "NetflowExporter"
@@ -27,6 +39,22 @@ class NetflowExporter(ConfigComponent):
         else:
             network_instance = self.kwargs.pop("network_instance")
             self.kwargs["network_instance"] = network_instance.name
+
+        # Resolve source_interface
+        if "source_interface" in self.kwargs:
+            print(f"Resolving source_interface for NetflowExporter {self.kwargs}")
+            #print(self.kwargs.pop("source_interface"))
+            si = self.kwargs.pop("source_interface")
+            print(type(si))
+            if isinstance(si, type(None)):
+                pass
+            elif isinstance(si, str):
+                ref = ReferenceTo(pointer=f"interfaces.{si}")
+                self.kwargs["source_interface"] = ref
+
+            elif isinstance(si, Interface):
+                ref = ReferenceTo(pointer=f"interfaces.{si.name}")
+                self.kwargs["source_interface"] = ref
 
 class NetflowExporterOptions(ConfigComponent):
     type = "NetflowExporterOptions"
