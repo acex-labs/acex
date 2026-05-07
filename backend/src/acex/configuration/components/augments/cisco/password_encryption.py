4da20d7a8e9b872ed75a2f592c3f6c559fd9723d
= <johan.lahti@acebit.se>
2026-05-07 18:11:55 +0200

    feat: cisco service password encryption

---
+++ backend/src/acex/configuration/components/augments/cisco/password_encryption.py
@@ -0,0 +1,22 @@
+"""
+Service password encryption.
+"""
+from typing import Literal, Optional
+
+from acex.configuration.components.augments.base import Augment
+from acex.configuration.components.system import SystemConfig
+from acex_devkit.models import AttributeValue
+from acex_devkit.models.composed_configuration import AugmentAttributes
+
+
+class CiscoServicePasswordEncryptionAttributes(AugmentAttributes):
+    "Enables service password encryption."
+    type: Literal["cisco.service_password_encryption"] = "cisco.service_password_encryption"
+    enabled: Optional[AttributeValue[bool]] = None
+
+
+class CiscoServicePasswordEncryption(Augment):
+    type = "cisco.service_password_encryption"
+    model_cls = CiscoServicePasswordEncryptionAttributes
+    valid_targets = (SystemConfig, )
+    default_vendor = "cisco"
