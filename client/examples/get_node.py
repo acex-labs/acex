9073961d706bd2b8f6237c6ed00d1918cc00dba9
Johan Lahti <johan.lahti@acebit.se>
2026-06-04 08:31:14 +0200

    wip

--- client/examples/get_node.py
+++ client/examples/get_node.py
@@ -2,11 +2,11 @@
 from acex_client.acex.acex import Acex
 
 
-# a = Acex(baseurl = "http://127.0.0.1/")
-a = Acex(baseurl = "https://api.auto.ngninfra.net/", verify=False)
+a = Acex(baseurl = "http://127.0.0.1/")
+# a = Acex(baseurl = "https://api.auto.ngninfra.net/", verify=False)
 
 
-node = a.nodes.get("2")
+node = a.node_instances.get("5")
+
 
 print(node)
-
