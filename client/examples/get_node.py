7be799db2c0d683807a3d858d345d947f23a76be
Johan Lahti <johan.lahti@acebit.se>
2026-06-22 14:29:30 +0200

    fix: new client

--- client/examples/get_node.py
+++ client/examples/get_node.py
@@ -1,11 +1,14 @@
 
 from acex_client.acex.acex import Acex
 
-from acex_client.acex.resources.credential import Credentials
+
 
 a = Acex(
     baseurl = "http://127.0.0.1:8080/",
     verify=False
 )
-# a = Acex(baseurl = "https://api.auto.ngninfra.net/", verify=False)
+
+
+print(a.node_instances.get("1"))
+
 
