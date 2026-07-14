d03a8cb808524cb2dd4b6cd4d9e4e3d650ce867e
johan-lahti <59826785+johan-lahti@users.noreply.github.com>
2026-07-14 13:07:12 +0200

    Update app.py

--- docs/examples/example14_cisco_augments/app.py
+++ docs/examples/example14_cisco_augments/app.py
@@ -29,8 +29,8 @@ ae = AutomationEngine(
 )
 
 ae.set_influxdb(
-    url="http://eru614:8181",
-    token="apiv3_lVnJoRcEfuAERSh_bmP1vj5uTY5V6_wq7mNuoZkKDFpDY-abn2Zvc5L-BDTHpjSL6yx0vW_1lg-xamJ5jixYoA",
+    url=os.getenv("INLFUXDB_SERVER_URL"),
+    token=os.getenv("INFLUX_TOKEN"),
     database="acex",
     content_encoding="gzip",
     version="v3"
