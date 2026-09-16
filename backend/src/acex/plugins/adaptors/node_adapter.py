6b1234e5dae5608cae1f2433f87fa8870e2e0d52
Wilhelm Törnblom <wilhelm.tornblom@acebit.se>
2026-09-16 13:00:46 +0200

    feat: ip addresses on node instances

--- backend/src/acex/plugins/adaptors/node_adapter.py
+++ backend/src/acex/plugins/adaptors/node_adapter.py
@@ -13,11 +13,12 @@ class NodeAdapter(AdapterBase):
         if hasattr(self.plugin, "get"):
             return self.plugin.get(id)
 
-    def query(self, filters: dict = None, limit: int = 100, offset: int = 0) -> list[Node]:
+    def query(self, filters: dict = None, extra_filters: list = None, limit: int = 100, offset: int = 0) -> list[Node]:
         if hasattr(self.plugin, "query"):
             return self.plugin.query(
                 filters,
                 options=[joinedload(Node.logical_node)],
+                extra_filters=extra_filters,
                 limit=limit,
                 offset=offset,
             )
